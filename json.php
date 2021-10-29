1a61188dfb7d7cbd561ed7bf50a5552cf4274856
faisalahammed044274 <setu2014@gmail.com>
2021-10-30 02:43:28 +0600

    json

--- json.php
+++ json.php
@@ -1,5 +1,9 @@
 <?php
 header("content-type","application/json");
-$json = file_get_contents('json.json');
+//$json = file_get_contents('json.json');
+
+$MyArray = array("firstName" => " Mike", "LastName" =>"Smith");
+$json = json_encode($MyArray);
+
 echo $json;
 ?>
